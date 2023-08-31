e698f7d3733ee8d0bf22e63d9a56586b89903d46
Talha Nasir <talha.nasir@gmail.com>
2023-08-31 21:04:01 +0500

    feat: Add video functionality User can add video User can see all his previous uploaded videos

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,12 +1,45 @@
 const { PrismaClient } = require('@prisma/client')
-const prisma =  new PrismaClient();
+const prisma = new PrismaClient();
 const asyncHandler = require('express-async-handler')
+const multer = require('multer')
 
-
-const uploadVideo = asyncHandler(async(req,res) => {
+const uploadVideo = asyncHandler(async (req, res) => {
+  const { title, description } = req.body;
   console.log("I'm in Video Upload")
+  console.log(req.file)
+
+  const uploaderId = req.user.id;
+
+  const newVideo = await prisma.video.create({
+    data: {
+      title,
+      description,
+      filePath: req.file.path,
+      uploader: {
+        connect: { id: uploaderId },
+      },
+    },
+  });
+
+  if (newVideo) {
+    console.log('Video uploaded:', req.file.filename);
+    return res.redirect('/userIndex/redirect-to-video');
+  } else {
+    res.status(500).send("Video Uploading issue");
+  }
+
+})
+
+const redirectToVideo = asyncHandler(async(req,res) => {
+    const userVideos = await prisma.video.findMany({
+      where: {
+        uploaderId: req.user.id,
+      }
+    })
+  res.render('video', { videos: userVideos });
 })
 
 module.exports = {
-  uploadVideo
+  uploadVideo,
+  redirectToVideo
 }
--- routes/userIndexRoutes.js
+++ routes/userIndexRoutes.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+const { uploadVideo, redirectToVideo } = require('../controller/videoController');
+const multer = require('multer');
 const validateToken = require('../middleware/validateJwt');
-const { uploadVideo } = require('../controller/videoController');
+const path = require('path');
 
-router.get('/index', validateToken, (req, res) => {
-  res.render('user_index');
+router.get('/index', (req, res) => {
+	res.render('user_index');
 });
 
-router.get('/redirect-to-video', validateToken, (req, res) => {
-  res.render('video'); 
+router.get('/redirect-to-video', validateToken, redirectToVideo);
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads/');
+	},
+	filename: (req, file, cb) => {
+		const extension = path.extname(file.originalname);
+		cb(null, `${Date.now()}${extension}`);
+	}
 });
 
-router.post('/uploadVideo', validateToken, uploadVideo )
+const upload = multer({ storage: storage });
+router.post('/uploadVideo', validateToken, upload.single('uploadFile'), uploadVideo);
 
 module.exports = router;
